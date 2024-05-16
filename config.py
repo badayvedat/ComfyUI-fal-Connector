eb4feda58b5190e8c451c24646277ea0369c943f
badayvedat <badayvedat@gmail.com>
2024-05-16 04:10:28 +0300

    refactor: update get_headers function to use get_fal_api_key

--- config.py
+++ config.py
@@ -23,25 +23,37 @@ def get_fal_endpoint():
 
 @functools.cache
 def get_headers():
-    config = get_fal_config()
-    return {"Authorization": f"Key {config['fal']['api_key']}"}
+    api_key = get_fal_api_key()
+    return {"Authorization": f"Key {api_key}"}
 
 
-def set_fal_credentials():
+def get_fal_api_key():
     from fal_client.auth import fetch_credentials
 
     try:
         api_key = fetch_credentials()
     except MissingCredentialsError:
         api_key = None
-
+    
+    try:
+        if not api_key:
+            config = get_fal_config()
+            api_key = config["fal"]["api_key"]
+    except Exception as e:
+        print(e)
+        raise MissingCredentialsError("No FAL API key found.")
+    
     if not api_key:
-        config = get_fal_config()
+        raise MissingCredentialsError("No FAL API key found.")
 
-        api_key = config["fal"]["api_key"]
-        os.environ["FAL_KEY"] = api_key
+    return api_key
+        
 
-        # Backwards compatibility
-        key_id, key_secret = api_key.split(":")
-        os.environ["FAL_KEY_ID"] = key_id
-        os.environ["FAL_KEY_SECRET"] = key_secret
+def set_fal_credentials():
+    api_key = get_fal_api_key()
+    os.environ["FAL_KEY"] = api_key
+
+    # Backwards compatibility
+    key_id, key_secret = api_key.split(":")
+    os.environ["FAL_KEY_ID"] = key_id
+    os.environ["FAL_KEY_SECRET"] = key_secret
